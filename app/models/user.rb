00e1f38509458e8fc051b2c8f45cbc192b5bb451
loic rosier <rosierloicdu14@gmail.com>
2022-01-04 16:33:33 +0100

    devise systeme & user controller ext

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,12 @@ class User < ApplicationRecord
   has_many :user_skills
   has_many :offers
   has_many :matches
+  # pour les choix dans les form
+  RECRUTOR = {oui: true, non: false}
+  GENDER = ['homme', 'femme']
+  ##
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :timeoutable
 end
