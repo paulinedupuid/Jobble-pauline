98b1a39314dca6388cec636b8f55fe7eda9b571f
Pauline Dupuid <pauline.dupuid@gmail.com>
2022-01-05 17:58:11 +0100

    first steps on the index method

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,7 +1,7 @@
 class OffersController < ApplicationController
   def index
-    # @offers = Offer.all
-    # @user = current_user
-    # raise
+    @user = current_user
+    @offers = Offer.where(job_id: @user.jobs.ids)
+    raise
   end
 end
