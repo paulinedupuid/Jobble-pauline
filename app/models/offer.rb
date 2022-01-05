98d9ca4e13f1157d26fdeb92e7a334b916b94369
Pauline Dupuid <pauline.dupuid@gmail.com>
2022-01-05 12:24:37 +0100

    added dependencies in offer and user models

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,8 +1,8 @@
 class Offer < ApplicationRecord
   belongs_to :job
   belongs_to :user
-  has_many :matches
-  has_many :offer_skills
+  has_many :matches, dependent: :destroy
+  has_many :offer_skills, dependent: :destroy
 
   validates :title, presence: true, length: { maximum: 20 }
   validates :description, presence: true, length: { in: 20..1200 }
