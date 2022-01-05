e9f6b535c14d5b6d403be2eb9ee33ba317b42dc2
Pauline Dupuid <pauline.dupuid@gmail.com>
2022-01-05 11:26:03 +0100

    route created, update method created, show created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,17 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
 
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :first_name, :last_name, :city, :gender, :birthdate)
+  end
 end
