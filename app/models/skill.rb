07e31598c0b2bf6cecb4c107ee47c2fa2c4fa8ea
Rowenda <66358738+Rowenda@users.noreply.github.com>
2022-01-05 12:05:07 +0100

    Update skill.rb

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,6 +1,7 @@
 class Skill < ApplicationRecord
+  has_many :user_skills
+  has_many :offer_skills
   has_many :users, through: :user_skills
-  # has_many :offer_skills
 
   validates :name, presence: true
 end
