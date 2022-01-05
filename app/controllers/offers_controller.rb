d723071380510e862274a0593eca88739107de60
Pauline Dupuid <pauline.dupuid@gmail.com>
2022-01-05 16:34:10 +0100

    route, action and view created

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,2 +1,7 @@
 class OffersController < ApplicationController
+  def index
+    # @offers = Offer.all
+    # @user = current_user
+    # raise
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,5 @@ Rails.application.routes.draw do
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: [:show, :update]
+  resources :offers, only: [:index]
 end
