1b1794ed16abef0bf624455c81917a6eb05b2b28
Rowenda <66358738+Rowenda@users.noreply.github.com>
2022-01-06 12:08:08 +0100

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,7 @@ Rails.application.routes.draw do
 
   resources :users, only: %i[show]
   resources :messages, only: %i[show]
-
+  resources :offers, only: [:index, :show]
   end
 ####################################
 # a tester
@@ -29,4 +29,4 @@ Rails.application.routes.draw do
   #     patch :update_user_jobs
   #   end
   # end
-  resources :offers, only: [:index, :show]
+
