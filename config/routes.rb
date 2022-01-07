0477ee0a2f346316952209529f8c5e17de517f4f
Pauline Dupuid <pauline.dupuid@gmail.com>
2022-01-07 12:18:33 +0100

    nested matches route into offers

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :messages, only: %i[show]
 
-  resources :offers, only: %i[index show]
+  resources :offers, only: %i[index show] do
+    resources :matches, only: :create
+  end
 end
