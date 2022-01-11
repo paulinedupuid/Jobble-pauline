f44d8194e431e9bdb9734bfd34222f97f848505a
loic rosier <rosierloicdu14@gmail.com>
2022-01-11 11:20:53 +0100

    notif message

--- app/javascript/channels/chatroom_channel.js
+++ app/javascript/channels/chatroom_channel.js
@@ -1,6 +1,7 @@
 import consumer from "./consumer";
 
 const initChatroomCable = () => {
+  const nonVu = document.getElementById('message-notif');
   const messagesContainer = document.getElementById('messages');
   if (messagesContainer) {
     const id = messagesContainer.dataset.chatroomId;
@@ -10,14 +11,12 @@ const initChatroomCable = () => {
         console.log(messagesContainer)
         messagesContainer.insertAdjacentHTML('beforeend', data);
         // called when data is broadcast in the cable
-        const url = `http://localhost:3000/chatrooms/${id}`
+        // const url = `http://localhost:3000/chatrooms/${id}`
         // window.location.href = url
 
-        // const test = document.querySelector('.test');
-        // const notif = document.getElementById('message-notif');
-        // console.log(notif);
-        // test.innerHTML = notif.dataset.messageCount;
-            // a finir le inser
+        // console.log(nonVu.dataset.message);
+        // const aa = nonVu.dataset.message
+        // nonVu.innerHTML = aa;
 
 
       },
