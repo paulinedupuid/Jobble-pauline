d338c77cf837d5cd9ff957a3795ad950ccfa5e40
Pauline Dupuid <pauline.dupuid@gmail.com>
2022-01-11 11:00:09 +0100

    reject swipe and script conditionned to image

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -27,7 +27,7 @@ import "bootstrap";
 import { jobDisplay } from 'controllers/display_jobs';
 import { userInfoDisplay } from "../controllers/display_user_info";
 import { initChatroomCable } from '../channels/chatroom_channel';
-import { swipe } from '../plugins/swipe';
+import { swipeOffer } from '../plugins/swipe';
 
 
 document.addEventListener('turbolinks:load', () => {
@@ -38,7 +38,7 @@ document.addEventListener('turbolinks:load', () => {
   userInfoDisplay()
   jobDisplay()
   initChatroomCable()
-  swipe()
+  swipeOffer()
 
 
   // disable pinch-zoom on smartphone -------------------------
--- app/javascript/plugins/swipe.js
+++ app/javascript/plugins/swipe.js
@@ -1,56 +1,70 @@
-let image = document.querySelector(".offer-image");
+function swipeOffer() {
+  const image = document.querySelector(".offer-image");
 
-image.addEventListener("touchstart", startTouch, false);
-image.addEventListener("touchmove", swipe, false);
+  if (image) {
+    image.addEventListener("touchstart", startTouch, false);
+    image.addEventListener("touchmove", swipe, false);
+    console.log("connect")
 
-// Swipe Left / Right
-let initialX = null;
-let initialY = null;
+    // Swipe Left / Right
+    let initialX = null;
+    let initialY = null;
 
-function startTouch(e) {
-  initialX = e.touches[0].clientX;
-  initialY = e.touches[0].clientY;
-};
+    function startTouch(e) {
+      console.log("start")
+      initialX = e.touches[0].clientX;
+      initialY = e.touches[0].clientY;
+    };
 
-function swipe(e) {
-  if (initialX === null) {
-    return;
+    function swipe(e) {
+      if (initialX === null) {
+        return;
+      }
+
+      if (initialY === null) {
+        return;
+      }
+
+      let currentX = e.touches[0].clientX;
+      let currentY = e.touches[0].clientY;
+
+      let diffX = initialX - currentX;
+      let diffY = initialY - currentY;
+
+      if (Math.abs(diffX) > Math.abs(diffY)) {
+        console.log("hello")
+        // sliding horizontally
+        if (diffX > 0) {
+          // swiped left
+          console.log("swiped left");
+          const id = e.target.dataset.id
+          const url = `/offers/${id}/reject`
+          fetch(url, {
+            method: "POST",
+            headers: { "Accept": "text/html" },
+            body: ""
+          })
+            .then(window.location.href = "/offers")
+        } else {
+          // swiped right
+          console.log("swiped right");
+          const id = e.target.dataset.id
+          const url = `/offers/${id}/matches`
+          fetch(url, {
+            method: "POST",
+            headers: { "Accept": "text/html" },
+            body: ""
+          })
+          .then(window.location.href="/offers")
+        }
+      }
+
+      initialX = null;
+      initialY = null;
+
+      e.preventDefault();
+    };
   }
+}
 
-  if (initialY === null) {
-    return;
-  }
-
-  let currentX = e.touches[0].clientX;
-  let currentY = e.touches[0].clientY;
-
-  let diffX = initialX - currentX;
-  let diffY = initialY - currentY;
-
-  if (Math.abs(diffX) > Math.abs(diffY)) {
-    // sliding horizontally
-    if (diffX > 0) {
-      // swiped left
-      console.log("swiped left");
-    } else {
-      // swiped right
-      console.log("swiped right");
-      const id = e.target.dataset.id
-      const url = `/offers/${id}/matches`
-      fetch(url, {
-        method: "POST",
-        headers: { "Accept": "text/html" },
-        body: ""
-      })
-      .then(window.location.href="/offers")
-
-    }
-  }
-
-  initialX = null;
-  initialY = null;
-
-  e.preventDefault();
-};
-
-export { swipe }
+export { swipeOffer }
