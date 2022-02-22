7745e336597e4908634956c5655744470081decf
Pauline Dupuid <pauline.dupuid@gmail.com>
2022-02-22 14:50:45 +0100

    code cleaned

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,12 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :city, :recrutor, :description, :gender, :birthdate])
+    devise_parameter_sanitizer.permit(:sign_up,
+                                      keys: %i[first_name last_name city recrutor description gender birthdate])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :city, :recrutor, :description, :gender, :birthdate])
+    devise_parameter_sanitizer.permit(:account_update,
+                                      keys: %i[first_name last_name city recrutor description gender birthdate])
   end
 
   def default_url_options
--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,7 +1,6 @@
 class ChatroomsController < ApplicationController
   def show
     @chatroom = Chatroom.find(params[:id])
-    # @chatroom.messages.where()
     @message = Message.new
     @chatroom.messages.each do |message|
       unless message.user_id == current_user.id
@@ -9,7 +8,6 @@ class ChatroomsController < ApplicationController
         message.save!
       end
     end
-# ajout destroy si + de 200
     message_all = @chatroom.messages.all
     if message_all.size > 200
       message_all.each do |me|
